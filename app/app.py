d9a3b1fde078b83dcc947bdf05eb15e42380c574
MichelleJ22 <michelle.jones.atl86@gmail.com>
2026-05-14 19:47:32 -0400

    Update Flask app message

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "You did it!"
+    return "GitOps deployment successful from GitHub Actions and Argo CD!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
