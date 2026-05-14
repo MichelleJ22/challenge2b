8908cd801bde3e9a06e07b8ce9c0457eaecd394e
MichelleJ22 <michelle.jones.atl86@gmail.com>
2026-05-14 13:58:11 -0400

    Update Flask homepage text4

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "You're a cloud engineer!"
+    return "You're a cloud engineer today!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
