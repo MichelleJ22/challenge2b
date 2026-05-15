e032e1e088b00bdccaf3dbabf869c3cd2948fe8c
MichelleJ22 <michelle.jones.atl86@gmail.com>
2026-05-15 12:52:28 -0400

    Argo Text 3

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Good job this Friday afternoon!!"
+    return "Good job this Friday afternoon 3rd attempt!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
